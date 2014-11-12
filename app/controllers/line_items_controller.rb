723526347363905f398ebd31501632dd47e9317d
Matthew Kilgannon <m.kilgannon2@nuigalway.ie>
2014-11-12 12:55:56 +0000

    Added Ajax.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,6 +32,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
+        format.js
         format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
