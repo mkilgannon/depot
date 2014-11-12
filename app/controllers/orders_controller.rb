11e16242bfc6d741b80fda206248149266db0e06
Matthew Kilgannon <m.kilgannon2@nuigalway.ie>
2014-11-12 14:11:45 +0000

    Validations.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,6 +16,10 @@ class OrdersController < ApplicationController
   # GET /orders/new
   def new
     @order = Order.new
+    if @cart.line_items.empty?
+    redirect_to store_url, notice: "Your cart is empty"
+    return
+end
   end
 
   # GET /orders/1/edit
@@ -26,10 +30,15 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
-
+    @order.add_line_items_from_cart(@cart)
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        Cart.destroy(session[:cart_id])
+session[:cart_id] = nil
+
+format.html { 
+  redirect_to store_url, notice: 'Thank you for your order.'
+}
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
